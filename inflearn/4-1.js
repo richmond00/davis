7fe098f9565db89fc8fff9e981e7bcaf6e03cf2f
Dongwook Kim <dongwook227@gmail.com>
2022-12-24 20:48:18 -0500

    update 4-1

--- inflearn/4-1.js
+++ inflearn/4-1.js
@@ -1,10 +1,11 @@
-function solution(input) {
+function solution1(input) {
   var max = -1;
   var numsWithMax = [];
   var biggestNum = 0;
 
   for (let i = 0; i < input.length; i++) {
     var currentNum = input[i];
+
     var strArr = String(currentNum).split("");
     var sum = 0;
 
@@ -27,6 +28,57 @@ function solution(input) {
   return biggestNum;
 }
 
+function solution2(input) {
+  var max = -1;
+  var numWithMax = -1;
+
+  for (let i = 0; i < input.length; i++) {
+    var currentNum = input[i];
+
+    var sum = String(currentNum)
+      .split("")
+      .reduce((acc, curr) => (acc += Number(curr)), 0);
+
+    if (sum > max) {
+      max = sum;
+      numWithMax = currentNum;
+    } else if (sum === max) {
+      if (numWithMax < currentNum) {
+        numWithMax = currentNum;
+      }
+    }
+  }
+
+  return numWithMax;
+}
+
+function solution3(input) {
+  var max = -1;
+  var numWithMax = -1;
+
+  for (let i = 0; i < input.length; i++) {
+    var currentNum = input[i];
+    var sum = 0;
+
+    while (currentNum) {
+      var remainder = currentNum % 10;
+      sum += remainder;
+      currentNum = Math.floor(currentNum / 10);
+    }
+
+    if (sum > max) {
+      max = sum;
+      numWithMax = input[i];
+    } else if (sum === max) {
+      if (numWithMax < input[i]) {
+        numWithMax = input[i];
+      }
+    }
+  }
+
+  return numWithMax;
+}
+
 var input = [128, 460, 603, 40, 521, 137, 123];
-var output = solution(input);
+var output = solution2(input);
 console.log(output);
