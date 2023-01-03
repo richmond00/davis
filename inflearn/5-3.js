544cd04f4882a9628a53f6e8a4b0a925cf98b4b1
Dongwook Kim <dongwook227@gmail.com>
2023-01-03 07:39:17 -0500

    updated 5-3

--- inflearn/5-3.js
+++ inflearn/5-3.js
@@ -21,6 +21,25 @@ function solution(nums, m) {
   return counter;
 }
 
+function solution2(nums, m) {
+  var counter = 0;
+  var lt = 0;
+  var sum = 0;
+
+  for (let rt = 0; rt < nums.length; rt++) {
+    sum += nums[rt];
+
+    while (sum >= m) {
+      if (sum === m) {
+        counter++;
+      }
+      sum -= nums[lt];
+      lt++;
+    }
+  }
+  return counter;
+}
+
 var nums = [1, 2, 1, 3, 1, 1, 1, 2];
-var output = solution(nums, 6);
+var output = solution2(nums, 6);
 console.log(output);
